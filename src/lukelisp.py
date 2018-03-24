2591b0c796dadf70d48c92c1c04d91af9c58a6a4
Luke Fitzpatrick <lukedfitzpatrick@gmail.com>
2018-03-25 10:46:05 +1100

    command line args

--- src/lukelisp.py
+++ src/lukelisp.py
@@ -1,12 +1,18 @@
 from parser import *
 from environment import *
 from eval import *
+import argparse
 
 VERSION = "0.1"
 
-# todo read from command line
-VERBOSE_TOKENISER = False
-VERBOSE_PARSER = False
+# read command line arguments
+parser = argparse.ArgumentParser(description='LukeLisp')
+
+parser.add_argument('--verbose_tokeniser', action='store_true', help='Verbose tokeniser')
+parser.add_argument('--verbose_parser', action='store_true', help='Verbose parser')
+parser.add_argument('-i', action='store_true', help='Launch REPL')
+
+args = parser.parse_args()
 
 def runRepl():
     env = getStandardEnv()
@@ -14,7 +20,8 @@ def runRepl():
     print "LukeLisp " + VERSION
     while(True):
         program = raw_input("LukeLisp: ")
-        ast = parse(program, (VERBOSE_TOKENISER, VERBOSE_PARSER))
+        ast = parse(program, (args.verbose_tokeniser, args.verbose_parser))
         print evaluate(ast, env)
-        
-runRepl()        
+
+if(args.i):
+    runRepl()        
